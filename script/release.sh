ad24e19c56e8bd43f7cd6cbd04f404b24a11ef54
MirageCentury <victorien.vanroye@gmail.com>
2021-11-09 04:53:54 +0100

    feat(ci): Add release script to locally release

---
+++ script/release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+plugin="@semantic-release/github"
+plugin="${plugin},@semantic-release/commit-analyzer"
+plugin="${plugin},@semantic-release/release-notes-generator"
+plugin="${plugin},@semantic-release/changelog"
+plugin="${plugin},@semantic-release/git"
+plugin="${plugin}"
+
+npx semantic-release -b main -p "${plugin}" --no-ci
