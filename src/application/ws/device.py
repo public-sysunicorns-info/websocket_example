329a725645fda6a5e57492bffaa8d0a4025616ed
Victorien VANROYE <victorien.vanroye@gmail.com>
2021-11-18 18:02:47 +0100

    docs(ws/device): #13 Add description on device ws endpoint

--- src/application/ws/device.py
+++ src/application/ws/device.py
@@ -1,14 +1,16 @@
 from fastapi import WebSocket, Depends, APIRouter
 from dependency_injector.wiring import inject, Provide
 from starlette.websockets import WebSocketDisconnect
-from application import api
-from application.container import ApplicationContainer
 import logging
 
+from application.container import ApplicationContainer
 from application.service.device import DeviceService
 
+
+# Instanciate Logger
 logger = logging.getLogger(__package__)
 
+# Instanciate Router for FastAPI
 ws_router_device = APIRouter()
 
 @ws_router_device.websocket(
@@ -20,22 +22,38 @@ async def websocket_device_endpoint(
     api_key: str,
     device_service: DeviceService = Depends(Provide[ApplicationContainer.device_service])
 ):
-    print(device_service)
+    """
+    Websocket Endpoint that take api_key as path params and handle the device connection
+    """
+    # Authenticate the device base on api_key
     _authenticated, _device = await device_service.authenticate_device(device_api_key=api_key)
 
     if _authenticated:
+        # Handshake the connection of websocket if authentication success
         await websocket.accept()
+
+        # Register the connection of the device
         _connected = await device_service.connect_device(device=_device, websocket=websocket)
         logger.info(f"{_device=} {api_key=} Connected")
+
+        # Send hello payload
+        # TODO : Issue #14 - Create better hello payload with more information for the device 
+        # ( https://github.com/public-sysunicorns-info/websocket_example/issues/14 )
         await websocket.send_json({"device_id": _device.device_id})
+        
+        # Loop to wait reception of message from the device
         try:
             while True:
                 data = await websocket.receive_json()
-                print(data)
+                logger.debug(f"{_device.device_id=} {data=}")
         except WebSocketDisconnect as e:
+            # Websocket disconnect from the device or path
             await device_service.disconnect_device(device=_device, websocket=websocket)
         except RuntimeError as e:
+            # Unattented Runtime Error trigger device connection to force establisment of a new connection
             await device_service.disconnect_device(device=_device, websocket=websocket)
+            logger.exception(e)
     else:
+        # Close the connection due to authentication failed
         await websocket.close()
-        logger.info(f"{api_key=} Force Disconnected")
+        logger.info(f"Disconnected by authentication failed {api_key=}")
